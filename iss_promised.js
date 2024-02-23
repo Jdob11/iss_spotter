ae270341cdcaa8cd2959f9788c14f156c6910cbc
Jdob11 <jbdobson11@gmail.com>
2024-02-23 14:20:36 -0800

    chore: linted files

--- iss_promised.js
+++ iss_promised.js
@@ -5,25 +5,25 @@ const fetchMyIP = function() {
 };
 
 const fetchMyCoordByIP = function(body) {
-  const ip = JSON.parse(body).ip; 
+  const ip = JSON.parse(body).ip;
   return request(`http://ipwho.is/${ip}`);
 };
 
-const fetchISSFlyOverTimes = function (body) {
+const fetchISSFlyOverTimes = function(body) {
   const { latitude, longitude} = JSON.parse(body);
   return request(`https://iss-flyover.herokuapp.com/json/?lat=${latitude}&lon=${longitude}`);
 };
 
 const nextISSTimesForMyLocation = function() {
   return fetchMyIP()
-  .then(fetchMyCoordByIP)
-  .then(fetchISSFlyOverTimes)
-  .then(body => {
-    const { response } = JSON.parse(body);
-    return response;
-  });
-}
+    .then(fetchMyCoordByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(body => {
+      const { response } = JSON.parse(body);
+      return response;
+    });
+};
 
 module.exports = {
   nextISSTimesForMyLocation
-}
+};
