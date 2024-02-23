db64e1385d5d3bb047664b6aff87c27f91181aeb
Jdob11 <jbdobson11@gmail.com>
2024-02-23 10:18:24 -0800

    feat: Add formatFlyOverTimes function
    This function takes the output from the nextISSTimesForMyLocation function and formats it to be more readable for the user. It organizes the flyover times into a user-friendly format, improving the overall user experience.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const { fetchMyIP, fetchMyCoordByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
+const formatFlyOverTimes = function(flyOverTimes) {
+  for (const time of flyOverTimes) {
+    let date = new Date(time.risetime * 1000)
+    date = date.toString();
+    const seconds = time.duration;
+    console.log(`The ISS will next fly over your location on ${date} for a total of ${seconds} seconds.`)
+  }
+}
+
 nextISSTimesForMyLocation((error, flyOverTimes) => {
   if (error) {
     return console.log('It didn\'t work!', error);
   }
-  console.log(flyOverTimes);
+  formatFlyOverTimes(flyOverTimes);
 });
