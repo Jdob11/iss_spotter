2a701873e254c4bce291a7ea3de4c5b2c09d2440
Jdob11 <jbdobson11@gmail.com>
2024-02-23 02:31:07 -0800

    linted

--- iss.js
+++ iss.js
@@ -1,6 +1,6 @@
 const request = require("request");
 
-const fetchMyIP = function(callback) { 
+const fetchMyIP = function(callback) {
   request('https://api.ipify.org?format=json', (error,response,body) => {
     if (error) {
       callback(error, null);
@@ -9,6 +9,7 @@ const fetchMyIP = function(callback) {
     const ip = JSON.parse(body);
     callback(null, ip);
   }
-  )};
+  );
+};
 
 module.exports = { fetchMyIP };
