c8135eb8b30a6388fd00bfcf277525b2e403ae94
Jdob11 <jbdobson11@gmail.com>
2024-02-23 10:04:21 -0800

    chore: remove obsolete testing function calls as all functions are not called in nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -1,32 +1,5 @@
 const { fetchMyIP, fetchMyCoordByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-
-//   console.log('It worked! Returned IP:' , ip);
-// });
-
-// fetchMyCoordByIP('149.115.34.26', (error, coordinates) => {
-//   if (error) {
-//     console.log('It didn\'t work:', error.message);
-//     return;
-//   }
-
-//   console.log('It worked! Coordinates are:', coordinates);
-// });
-
-// fetchISSFlyOverTimes('149.115.34.26', (error, flyOverTimes) => {
-//   if (error) {
-//     console.log('It didn\'t work:', error.message);
-//     return;
-//   }
-  
-//   console.log('It worked! Pass times are:', flyOverTimes);
-// });
-
 nextISSTimesForMyLocation((error, flyOverTimes) => {
   if (error) {
     return console.log('It didn\'t work!', error);
